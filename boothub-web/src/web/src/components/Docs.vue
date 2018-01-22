1d7fff3319e01a1ae4cb21b48a1968fda83b38a6
Serban Iordache <iordache@hotmail.com>
2018-01-22 22:41:04 +0100

    update doc links

--- boothub-web/src/web/src/components/Docs.vue
+++ boothub-web/src/web/src/components/Docs.vue
@@ -6,12 +6,18 @@
     <br>You can choose whether the generated project should be created on GitHub or offered as a zip file for download.
 
     <h3>Tutorials and documentation</h3>
+    <b>BootHub</b>
     <ul>
-      <li><a href="http://doc.boothub.org/releases/latest/#user_guide">User guide</a></li>
-      <li><a href="http://doc.boothub.org/releases/latest/#Templates">Writing BootHub templates</a></li>
-      <li><a href="http://doc.boothub.org/releases/latest/#dev_guide">Developer guide</a></li>
-      <p/>
-      <li><a href="http://java-groovy.boothub.org/releases/latest/">The Java/Groovy template</a></li>
+      <li><a href="http://doc.boothub.org/releases/latest/#user_guide" target="_blank">User guide</a></li>
+      <li><a href="http://doc.boothub.org/releases/latest/#Templates" target="_blank">Writing BootHub templates</a></li>
+      <li><a href="http://doc.boothub.org/releases/latest/#dev_guide" target="_blank">Developer guide</a></li>
+    </ul>
+    <p/>
+    <b>Frequently used templates</b>
+    <ul>
+      <li><a href="http://java-groovy.boothub.org/releases/latest/" target="_blank">The Java/Groovy template</a></li>
+      <li><a href="http://simple-java.boothub.org/releases/latest/" target="_blank">The Simple-Java template</a></li>
+      <li><a href="http://simple-groovy.boothub.org/releases/latest/" target="_blank">The Simple-Groovy template</a></li>
     </ul>
   </div>
 </template>
