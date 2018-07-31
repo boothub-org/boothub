48a86bf8c6148931ec0eba92a30d70f68ef56dbb
Serban Iordache <iordache@hotmail.com>
2018-07-31 23:26:41 +0200

    update doc links

--- boothub-web/src/web/src/components/Docs.vue
+++ boothub-web/src/web/src/components/Docs.vue
@@ -8,20 +8,29 @@
     <h3>Tutorials and documentation</h3>
     <b>BootHub</b>
     <ul>
-      <li><a href="http://doc.boothub.org/releases/latest/#user_guide" target="_blank">User guide</a></li>
-      <li><a href="http://doc.boothub.org/releases/latest/#Templates" target="_blank">Writing BootHub templates</a></li>
-      <li><a href="http://doc.boothub.org/releases/latest/#on_premises" target="_blank">Installing BootHub on-premises</a></li>
-      <li><a href="http://doc.boothub.org/releases/latest/#dev_guide" target="_blank">Developer guide</a></li>
+      <li><a href="https://doc.boothub.org/releases/latest/#user_guide" target="_blank">User guide</a></li>
+      <li><a href="https://doc.boothub.org/releases/latest/#Templates" target="_blank">Writing BootHub templates</a></li>
+      <li><a href="https://doc.boothub.org/releases/latest/#on_premises" target="_blank">Installing BootHub on-premises</a></li>
+      <li><a href="https://doc.boothub.org/releases/latest/#dev_guide" target="_blank">Developer guide</a></li>
     </ul>
     <p/>
     <b>Frequently used templates</b>
     <ul>
-      <li><a href="http://java-groovy.boothub.org/releases/latest/" target="_blank">The Java/Groovy template</a></li>
-      <li><a href="http://simple-java.boothub.org/releases/latest/" target="_blank">The Simple-Java template</a></li>
-      <li><a href="http://simple-groovy.boothub.org/releases/latest/" target="_blank">The Simple-Groovy template</a></li>
-      <li><a href="http://simple-kotlin.boothub.org/releases/latest/" target="_blank">The Simple-Kotlin template</a></li>
-      <li><a href="http://meta-template.boothub.org/releases/latest/" target="_blank">The Meta-Template</a></li>
+      <li><a href="https://java-groovy.boothub.org/releases/latest/" target="_blank">The Java/Groovy template</a></li>
+      <li><a href="https://simple-java.boothub.org/releases/latest/" target="_blank">The Simple-Java template</a></li>
+      <li><a href="https://simple-groovy.boothub.org/releases/latest/" target="_blank">The Simple-Groovy template</a></li>
+      <li><a href="https://simple-kotlin.boothub.org/releases/latest/" target="_blank">The Simple-Kotlin template</a></li>
+      <li><a href="https://meta-template.boothub.org/releases/latest/" target="_blank">The Meta-Template</a></li>
+      <li><a href="https://github.com/boothub-org/boothub-template-gradle-plugin/blob/master/README.md" target="_blank">The Gradle-Plugin template</a></li>
+      <li><a href="https://slide-deck.boothub.org/releases/latest/" target="_blank">The Slide-Deck template</a></li>
     </ul>
+
+    <h3>Articles</h3>
+    <ul>
+      <li><a href="https://dzone.com/articles/how-to-bootstrap-your-open-source-projects" target="_blank">How to Bootstrap Your Open-Source Projects</a></li>
+      <li><a href="https://beryx.org/blog/2018-08-01-/add-code-snippets-to-your-presentations/" target="_blank">Add code snippets to your technical presentations</a></li>
+    </ul>
+
   </div>
 </template>
 
